fcd970422a63c1d499578dc2e1b9c74b46f895ef
huailing.ng <huailing.ng@digipen.edu>
2025-08-04 11:07:52 +0800

    feat: add Command interface as base for command pattern

---
+++ src/Command.java
@@ -0,0 +1,16 @@
+/**
+ * Represents a command that can be executed.
+ *
+ * <p>This interface follows the Command design pattern,
+ * where implementing classes encapsulate all the information needed
+ * to perform an action or trigger an event.</p>
+ */
+public interface Command {
+
+    /**
+     * Executes the command.
+     * <p>Implementing classes should define
+     * the specific action that occurs when this method is called.</p>
+     */
+    void execute();
+}
